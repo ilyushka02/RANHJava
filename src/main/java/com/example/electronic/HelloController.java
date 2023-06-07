c766d4825a2f5eef417e1a047fd0856885908323
Ilya Nasurllaev <ilyaevgenievi4@mail.ru>
2023-06-08 00:29:11 +0300

    correct code and add check field

--- src/main/java/com/example/electronic/HelloController.java
+++ src/main/java/com/example/electronic/HelloController.java
@@ -6,6 +6,7 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 
 import java.io.IOException;
@@ -17,6 +18,28 @@ public class HelloController {
         alert.setHeaderText(header);
         alert.setContentText(text);
         alert.showAndWait();
+    }
+
+    private void checkEvent(TextField field,TextField nextField , KeyEvent event){
+        if (!Character.isDigit(event.getCode().getCode()) &&
+                !Character.isISOControl(event.getCode().getCode())) {
+            if (!event.getCode().getChar().equals(",")) {
+                createAlert("Предупреждение",
+                        "Некорректные данные в поле ввода данных",
+                        "Проверьте корректность введенных данных в поле.",
+                        Alert.AlertType.WARNING
+                );
+            } else if (field.getText().indexOf(',') != -1){
+                createAlert("Предупреждение",
+                        "Некорректные данные в поле ввода данных",
+                        "Проверьте корректность введенных данных в поле.",
+                        Alert.AlertType.WARNING
+                );
+            }
+        }
+        if (event.getCode() == KeyCode.ENTER){
+            nextField.requestFocus();
+        }
     }
 
     @FXML
@@ -36,37 +59,45 @@ public class HelloController {
 
     @FXML
     void previewCheck(KeyEvent event) {
-        if (!Character.isDigit(event.getCode().getCode()) &&
-                !Character.isISOControl(event.getCode().getCode())) {
-            if (!event.getCode().getChar().equals(",")) {
-                createAlert("Предупреждение",
-                        "Некорректные данные в поле ввода данных",
-                        "Проверьте корректность введенных данных в поле\nпредыдущее показание",
-                        Alert.AlertType.WARNING
-                        );
-            } else if (previewTxt.getText().indexOf(',') != -1){
-                createAlert("Предупреждение",
-                        "Некорректные данные в поле ввода данных",
-                        "Проверьте корректность введенных данных в поле\nпредыдущее показание",
-                        Alert.AlertType.WARNING
-                );
-            }
-        }
+        checkEvent(previewTxt, currentTxt, event);
     }
 
     @FXML
     void currentCheck(KeyEvent event) {
-
+        checkEvent(currentTxt, priceTxt, event);
     }
 
     @FXML
     void priceCheck(KeyEvent event) {
+        if(previewTxt.getText().length() == 0 ||
+                currentTxt.getText().length()==0){
+            createAlert("Предупреждение",
+                    "Не заполненные данные",
+                    "Заполните предыдущие поля.",
+                    Alert.AlertType.WARNING
+            );
 
+            if (previewTxt.getText().length() == 0)
+                previewTxt.requestFocus();
+            else  currentTxt.requestFocus();
+        }
+        checkEvent(currentTxt, priceTxt, event);
+        countBtn.setDisable(false);
     }
 
     @FXML
     void countng(ActionEvent event) {
-
+        if (priceTxt.getText().trim().isEmpty() == false)
+            summaryTxt.setText("nice");
+        else{
+            countBtn.setDisable(true);
+            createAlert("Предупреждение",
+                    "Не заполненные данные",
+                    "Заполните данные о цене.",
+                    Alert.AlertType.WARNING
+            );
+            priceTxt.requestFocus();
+        }
     }
 
     @FXML
